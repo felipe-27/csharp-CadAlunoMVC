ed6dc4b450cb3c15d93737e13ab6d91226cfd52c
FELIPE NASCIMENTO SILVA <081240020@faculdade.cefsa.edu.br>
2026-03-04 21:53:33 -0300

    Configurando acesso ao banco de dados - aula 04-03

---
+++ CadAlunoMVC/Controllers/AlunoController.cs
@@ -0,0 +1,18 @@
+﻿using CadAlunoMVC.DAO;
+using CadAlunoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CadAlunoMVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // método importante para interagir com o usuário
+        public IActionResult Index()
+        {
+            AlunoDAO dao = new AlunoDAO();
+            List<AlunoViewModel> lista = dao.Listagem();
+            return View(lista);
+        }
+    }
+}
