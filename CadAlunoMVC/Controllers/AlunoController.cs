b5d788e00dade5bd94c1d446c3146b52625c190f
FELIPE NASCIMENTO SILVA <081240020@faculdade.cefsa.edu.br>
2026-03-04 22:42:22 -0300

    adicionando formulario com comentarios - aula 04-03

--- CadAlunoMVC/Controllers/AlunoController.cs
+++ CadAlunoMVC/Controllers/AlunoController.cs
@@ -1,6 +1,7 @@
 ﻿using CadAlunoMVC.DAO;
 using CadAlunoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace CadAlunoMVC.Controllers
@@ -13,6 +14,22 @@ namespace CadAlunoMVC.Controllers
             AlunoDAO dao = new AlunoDAO();
             List<AlunoViewModel> lista = dao.Listagem();
             return View(lista);
+        }
+
+        public IActionResult Create()
+        {
+            AlunoViewModel aluno = new AlunoViewModel();
+
+            // preencher informações direto para o usuário
+            aluno.DataNascimento = DateTime.Now;
+            return View("Form", aluno);
+        }
+
+        public IActionResult Salvar(AlunoViewModel aluno)
+        {
+            AlunoDAO dao = new AlunoDAO();
+            dao.Inserir(aluno);
+            return RedirectToAction("index");
         }
     }
 }
