741d89a1b40ca6331e83a9613ffdacebff2798cf
felipe-27 <felipens27@gmail.com>
2026-04-07 20:35:02 -0300

    HelperDAO com storage procedure - aula 07-04

--- CadAlunoMVC/DAO/HelperDAO.cs
+++ CadAlunoMVC/DAO/HelperDAO.cs
@@ -34,5 +34,38 @@ namespace CadAlunoMVC.DAO
                 }
             }
         }
+
+        // Stored Procedures
+        public static void ExecutaProc(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoBD.GetConexao())
+            {
+                using (SqlCommand comando = new SqlCommand(nomeProc, conexao))
+                {
+                    // determinando o tipo do comando como SP
+                    comando.CommandType = CommandType.StoredProcedure;
+                    if (parametros != null)
+                        comando.Parameters.AddRange(parametros);
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+        public static DataTable ExecutaProcSelect(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoBD.GetConexao())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(nomeProc, conexao))
+                {
+                    if (parametros != null)
+                        adapter.SelectCommand.Parameters.AddRange(parametros);
+                    
+                    // determinando o tipo do comando como SP
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    return tabela;
+                }
+            }
+        }
     }
 }
