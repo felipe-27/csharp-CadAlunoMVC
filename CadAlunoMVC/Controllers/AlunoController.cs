235130afa64bb3b52cbb559a6a1659c12cc66e8a
felipe-27 <felipens27@gmail.com>
2026-04-08 21:26:20 -0300

    Adicionar metodo de validação na Controller

--- CadAlunoMVC/Controllers/AlunoController.cs
+++ CadAlunoMVC/Controllers/AlunoController.cs
@@ -51,14 +51,21 @@ namespace CadAlunoMVC.Controllers
         {
             try
             {
-                AlunoDAO dao = new AlunoDAO();
-
-                if (Operacao == "I")
-                    dao.Inserir(aluno);
+                ValidaDados(aluno, Operacao);
+                if (ModelState.IsValid == false)
+                {
+                    ViewBag.Operacao = Operacao;
+                    return View("Form", aluno);
+                }
                 else
-                    dao.Alterar(aluno);
-
-                return RedirectToAction("index");
+                {
+                    AlunoDAO dao = new AlunoDAO();
+                    if (Operacao == "I")
+                        dao.Inserir(aluno);
+                    else
+                        dao.Alterar(aluno);
+                    return RedirectToAction("index");
+                }
             }
             catch (Exception ex)
             {
@@ -97,6 +104,29 @@ namespace CadAlunoMVC.Controllers
             {
                 return View("Error", new ErrorViewModel(erro.ToString()));
             }
+        }
+
+        private void ValidaDados(AlunoViewModel aluno, string operacao)
+        {
+            ModelState.Clear(); // limpa os erros criados automaticamente pelo Asp.net (que podem estar com msg em inglês)
+            AlunoDAO dao = new AlunoDAO();
+            if (aluno.Id <= 0)
+                ModelState.AddModelError("Id", "Id inválido!");
+            else
+            {
+                if (operacao == "I" && dao.Consulta(aluno.Id) != null)
+                    ModelState.AddModelError("Id", "Código já está em uso.");
+                if (operacao == "A" && dao.Consulta(aluno.Id) == null)
+                    ModelState.AddModelError("Id", "Aluno não existe.");
+            }
+            if (string.IsNullOrEmpty(aluno.Nome))
+                ModelState.AddModelError("Nome", "Preencha o nome.");
+            if (aluno.Mensalidade < 0)
+                ModelState.AddModelError("Mensalidade", "Campo obrigatório.");
+            if (aluno.CidadeId <= 0)
+                ModelState.AddModelError("CidadeId", "Informe o código da cidade.");
+            if (aluno.DataNascimento > DateTime.Now)
+                ModelState.AddModelError("DataNascimento", "Data inválida!");
         }
     }
 }
